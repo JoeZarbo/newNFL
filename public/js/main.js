cfbc3daf5ecd1db1c82cccd0547c8b5d4a0cd386
JoeZarbo <123481818+JoeZarbo@users.noreply.github.com>
2025-07-29 12:38:50 -0400

    Created start function

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,7 @@
 const canvas = document.getElementById('bug');
 const ctx = canvas.getContext('2d');
+const bold = new FontFace('bold', 'url("/fonts/ARIBLK.TTF")');
+const italic = new FontFace('italic', 'url("/fonts/ARIALI.TTF")');
 const logoCache = {};
 let isBusy = false;
 
@@ -16,5 +18,24 @@ const background = {
 };
 
 function start() {
-    
+    const fontPromises = [
+        bold.load(),
+        italic.load()
+    ];
+    const imagePromises = preloadLogos();
+    Promise.all([...fontPromises, ...imagePromises]).then((loadedAssets) => {
+        document.fonts.add(loadedAssets[0]);
+        document.fonts.add(loadedAssets[1]);
+        draw();
+    })
+}
+
+function preloadLogos() {
+
+}
+
+function draw() {
+    bold.load();
+    italic.load();
+    background.draw();
 }
